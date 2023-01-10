d096ae22e780e82b3ee59c356227964b08ad30c0
Legacynnn <danmelomour@gmail.com>
2023-01-10 17:49:43 -0300

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
+
+
+function Hero() {
+  return (
+    <section 
+    id='home'
+    className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-1`} >
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Client from "./Client";
+import CTA from "./CTA";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonails from "./Testimonails";
+
+export {
+  NavBar,
+  Billing,
+  Business,
+  Button,
+  CTA,
+  CardDeal,
+  Client,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonails
+}
