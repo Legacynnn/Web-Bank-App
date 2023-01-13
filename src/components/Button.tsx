f59d32fbbad68ad5878015fea99677a23bad29cc
Legacynnn <danmelomour@gmail.com>
2023-01-13 00:57:49 -0300

    Business Commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Button() {
+interface StylesProps {
+  styles: string
+}
+
+function Button({styles}: StylesProps) {
   return (
-    <div>Button</div>
+    <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+      Get Started
+    </button>
   )
 }
 
